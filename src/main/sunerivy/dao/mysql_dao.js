ca30edbee8828dcfe52c00c902de8b90f48c11c8
zhou_jiafeng@aliyun.com <zhou_jiafeng@aliyun.com>
2018-08-31 20:48:04 +0800

    update

--- src/main/sunerivy/dao/mysql_dao.js
+++ src/main/sunerivy/dao/mysql_dao.js
@@ -1,6 +1,7 @@
 let {dbConfig} = require('../tool/config_util');
 let dbObj = dbConfig['mysql'];
 let connection;
+let pool;
 
 void function() {
     let host = dbObj.host;
@@ -9,38 +10,34 @@ void function() {
     let database = dbObj.database;
 
     let mysql      = require('mysql');
-    connection = mysql.createConnection({
-        host     : host,
+    pool = mysql.createPool({
+        host     : host,
         user     : username,
         password : password,
         database : database
     });
-    
-    connection.connect((error) => {
-        if(error){
-            throw error;
-        }else{
-            console.log('[db] [mysql] connection success!');
-        }
-    });
-    let sql = 'select * from user';
-    let param = [];
-    connection.query( sql , param , function(error,res){
-        if(error){
-            throw error;
-        }
-        return res;
-    });
-    return connection;
+
+    // connection.connect((error) => {
+    //     if(error){
+    //         throw error;
+    //     }else{
+    //         console.log('[db] [mysql] connection success!');
+    //     }
+    // });
 }();
 
 let query = (sql, param) => {
-    connection.query( sql , param , function(error,res){
-        if(error){
-            throw error;
-        }
-        return res;
-    });
+    let result ;
+    pool.getConnection(function(error, connection){
+        connection.query( sql , param , function(error,res){
+            if(error){
+                throw error;
+            }
+            result = res;
+        });
+        connection.release();
+    })
+    return result;
 }
 
 exports.query = query;
